adeda61712fb35c7d0beea5d0fe8ba7a55bbf29a
Bruno Santos <brunoadsjp@gmail.com>
2024-04-14 18:43:17 -0300

    add config cors in java api and create react project

--- produtos/src/main/java/br/com/api/produtos/controller/ProdutoController.java
+++ produtos/src/main/java/br/com/api/produtos/controller/ProdutoController.java
@@ -3,15 +3,10 @@ package br.com.api.produtos.controller;
 import br.com.api.produtos.model.Produto;
 import br.com.api.produtos.service.ProdutoService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProdutoController {
     
     private final ProdutoService produtoService;
